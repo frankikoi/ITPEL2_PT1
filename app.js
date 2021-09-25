80c9e13ddae5c55cb96a3a2aa5fd535de0dcff95
Azel Opeña <azel.opena22@gmail.com>
2021-09-25 16:32:35 +0800

    Commit first

---
+++ app.js
@@ -0,0 +1,42 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+const cancelButton = document.querySelector('#btn-clear');
+const addButton = document.querySelector('#btn-add');
+const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertInputControl = document.querySelector('ion-alert-controller');
+
+let myTotalExpenses = 0;
+
+const clear =() => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
+
+cancelButton.addEventListener('click', clear);
+
+addButton.addEventListener('click', () => {
+    console.log("Expense added!");
+    const reasonEntered = reasonInput.value;
+    const amountEntered = amountInput.value;
+    
+
+
+    if (reasonEntered.trim().length <= 0 || amountEntered <= 0 ||
+        amountEntered.trim().length <= 0){
+            alert("Please input reason and amount!")
+           
+            return;
+        }    
+        console.log(reasonEntered, amountEntered);
+
+        const newExpenses = document.createElement('ion-item');
+        newExpenses.textContent = reasonEntered + ': P' + amountEntered;
+        expensesList.appendChild(newExpenses);
+
+       
+        myTotalExpenses += +amountEntered;
+        
+        totalExpensesOutput.textContent = myTotalExpenses;
+        clear();
+});
